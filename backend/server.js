ba322c87544e6e62f56008dcb05fda64dd382fc7
knightofcookies <101050984+knightofcookies@users.noreply.github.com>
2025-10-08 04:13:35 +0530

    Comment out dotenv config in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const { Sequelize, DataTypes, Op } = require("sequelize");
 const cors = require("cors");
